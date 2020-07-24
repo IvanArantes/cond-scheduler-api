d4a00e642d626c1c50b43ec88cb6279e418352d3
Ivan Arantes <iarantes@haivision.com>
2020-07-24 15:36:28 -0300

    fixed url

--- app/config/databaseConfig.js
+++ app/config/databaseConfig.js
@@ -1,3 +1,3 @@
 module.exports = Object.freeze({
-  DBURL: "mongodb://cond-scheduler-db:M23zLCyb2kFCXfoprpTPsqK82iWFq8RerzKjfAZ1pu41kYCgh6lUVUquLyFXjRQxOUzmjrdNiUtibbZXaasKzg==@cond-scheduler-db.mongo.cosmos.azure.com:10255/?ssl=true&appName=@cond-scheduler-db@"
+  DBURL: ""
 });
